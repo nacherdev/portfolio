6c48b308e58b8fb0462a738e918ebd808527931e
Mohammad Nacher Rahman Akter <nacheruddin22@gmail.com>
2025-11-22 19:36:40 +0000

    update all

---
+++ app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+function Header (){
+    return (
+        <div>
+        <header>
+            <li><a href="#proyectos">Proyectos</a></li>
+            <li>Estudios</li>
+            <li>Experiencia</li>
+            <li>Sobre mi</li>
+        </header>
+        </div>
+    )
+}
+
+
+export default Header; 
---
+++ app/components/Main.tsx
@@ -0,0 +1,10 @@
+function Main(){
+    return (
+        <div id="proyectos">
+            <h2>Proyecto 1</h2>
+            <h2>Proyecto 2</h2>
+        </div>
+    )
+}
+
+export default Main;
